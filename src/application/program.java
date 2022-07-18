5a06c602ba04850949d2b3d3d88552f33d73ae2a
Eduardo <eduardocordeirosites@gmail.com>
2022-07-18 14:00:27 -0300

    first commit

---
+++ src/application/program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		
+		System.out.println("Enter product data:");
+		System.out.println("Product Name:");
+		product.name = sc.nextLine();
+		System.out.println("Price:");
+		product.price = sc.nextDouble();
+		System.out.println("Quantity:");
+		product.quantity = sc.nextInt();
+		
+		System.out.println(product);
+		System.out.println(product.toString());
+		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		
+		sc.close();
+	}
+
+}
